f8ee7c18f63b3d45dc36a131b2e19403d0f6f6eb
Manali Mathur <a.manalimathur@gmail.com>
2021-08-12 05:22:58 +0530

    add sidebar component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 // components
 import Header from './components/Header/Header';
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
     </div>
   );
 }
